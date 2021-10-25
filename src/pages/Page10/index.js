fdeba6862e9895a69ebd152dae44ab500aa70013
DESKTOP-IQOISR4\Center <sinhworking@gmail.com>
2021-10-26 03:03:22 +0700

    page 10

--- src/pages/Page10/index.js
+++ src/pages/Page10/index.js
@@ -1,10 +1,7 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import audioPlayer from "helper/audioPlayer";
 import audios from "assets/audios/index";
-
-import { setTimeOutAddClass } from "helper/setTimeOutControlClass";
-
 import "./styles.scss";
 import TitleMeeting from "components/TitleMeetting/index";
 import images from "assets/images/index";
@@ -12,22 +9,23 @@ import ConversationTK from "components/ConversationTK/index";
 
 const Page10 = (props) => {
 	const { onPushAction } = props;
-
-	const [active, setActive] = useState("");
-
-	const redBlockRef = useRef();
-
-	const { currentPage, currentStep, currentRecord, prevRecord } = useSelector(
+	const { currentPage, currentStep, currentRecord } = useSelector(
 		(state) => state.app
 	);
 
-	const { playAudio, pauseAudio } = audioPlayer;
+	const { playAudio } = audioPlayer;
 
 	const clickEventName = "page10";
 
 	const clickHandler = (e, op) => {
 		onPushAction(e, op.actionType, op);
 	};
+
+	const [image1, setImage1] = useState(images.common.leftTeacher);
+	const [image2, setImage2] = useState(images.common.rightKid);
+
+	const [textT1, setTextT1] = useState(null);
+	const [textT2, setTextT2] = useState(null);
 
 	useEffect(() => {
 		if (currentRecord.length > 0) {
@@ -37,24 +35,27 @@ const Page10 = (props) => {
 				recordEventData.eventPageStep === currentStep &&
 				recordEventData.eventName === clickEventName
 			) {
-				console.log(`runRecordEvent`, recordEventData);
-				if (recordEventData.eventData.active) {
-					setActive("active");
-					//setTimeOutAddClass('red-block','active',2000)
-				} else {
-					setActive("");
-				}
 				if (recordEventData.eventData.playAudio) {
-					//playAudio
 					let audioUrl = audios[recordEventData.eventData.playAudio];
 					playAudio(audioUrl);
+				}
+				if (recordEventData.eventData.imageG1) {
+					setImage1(recordEventData.eventData.imageG1);
+				}
+				if (recordEventData.eventData.imageG2) {
+					setImage2(recordEventData.eventData.imageG2);
+				}
+				if (recordEventData.eventData.textT1) {
+					setTextT1(recordEventData.eventData.textT1);
+				}
+				if (recordEventData.eventData.textT2) {
+					setTextT2(recordEventData.eventData.textT2);
 				}
 			}
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [currentRecord]);
 
-	console.log("render page10");
 	return (
 		<div className="page10">
 			<h1>this is page {currentPage}</h1>
@@ -102,19 +103,18 @@ const Page10 = (props) => {
 			>
 				play audio and animation
 			</button>
-
-			{/* <div ref={redBlockRef} className={`red-block ${active}`}>red block</div> */}
-
 			<div className="page-wrraper">
 				<TitleMeeting bgTitle={images.page1.titleMeeting} />
 				<ConversationTK
 					page="page10"
-					object1={images.common.leftTeacher}
-					object2={images.common.rightKid}
-					objectG1={images.common.leftTeacherGif}
-					objectG2={images.common.rightKid}
-					text1={images.page10.textTeacher10}
-					text2={images.page10.textKid10}
+					image1={image1}
+					image2={image2}
+					imageG1={images.common.leftTeacherGif}
+					imageG2={images.common.rightKid}
+					text1={textT1}
+					textGT1={images.page10.textTeacher10}
+					text2={textT2}
+					textGT2={images.page10.textKid10}
 					audio1="teacherAudio10"
 					audio2="kidAudio10"
 					clickHandler={clickHandler}
