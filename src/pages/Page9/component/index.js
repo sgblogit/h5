6e1841faf0d37dcddf45d5ffd714ea1083992550
DESKTOP-IQOISR4\Center <sinhworking@gmail.com>
2021-10-26 02:53:04 +0700

    page 9

--- src/pages/Page9/component/index.js
+++ src/pages/Page9/component/index.js
@@ -1,106 +1,102 @@
-import React, { useState } from "react";
+import React from "react";
 function Family(props) {
-  const {
-    brotherGif,
-    dadGif,
-    momGif,
-    clickHandler,
-    meGif,
-    audioMe,
-    audioBrother,
-    audioMom,
-    audioDad,
-    page,
-    people,
-    dadImg,
-    momImg,
-    brotherImg,
-    meImg
-  } = props;
-  const [img1, setImageDad] = useState(dadGif);
-  const [img2, setImageMom] = useState(momGif);
-  const [img3, setImageBrother] = useState(brotherGif);
-  const [img4, setImageMe] = useState(meGif);
+	const {
+		brotherGif,
+		dadGif,
+		momGif,
+		clickHandler,
+		meGif,
+		audioMe,
+		audioBrother,
+		audioMom,
+		audioDad,
+		page,
+		people,
+		img1,
+		img2,
+		img3,
+		img4,
+	} = props;
 
-  return (
-    <div className="main-content">
-      <div className="list-item">
-        <div className="grid-item">
-          <div className="dad item">
-            <img
-              src={img1}
-              alt={img1}
-              onClick={(e) => {
-                setImageDad(dadImg);
-                clickHandler(e, {
-                  actionType: "fireEvent",
-                  eventName: page,
-                  eventData: {
-                    playAudio: audioDad,
-                    active: true,
-                  },
-                });
-              }}
-            />
-          </div>
-          <div className="mom item">
-            <img
-              src={img2}
-              alt={img2}
-              onClick={(e) => {
-                setImageMom(momImg);
-                clickHandler(e, {
-                  actionType: "fireEvent",
-                  eventName: page,
-                  eventData: {
-                    playAudio: audioMom,
-                    active: true,
-                  },
-                });
-              }}
-            />
-          </div>
-          <div className="brother item">
-            <img
-              src={img3}
-              alt={img3}
-              onClick={(e) => {
-                setImageBrother(brotherImg);
-                clickHandler(e, {
-                  actionType: "fireEvent",
-                  eventName: page,
-                  eventData: {
-                    playAudio: audioBrother,
-                    active: true,
-                  },
-                });
-              }}
-            />
-          </div>
-          <div className="me item">
-            <img
-              src={img4}
-              alt={img4}
-              onClick={(e) => {
-                setImageMe(meImg);
-                clickHandler(e, {
-                  actionType: "fireEvent",
-                  eventName: page,
-                  eventData: {
-                    playAudio: audioMe,
-                    active: true,
-                  },
-                });
-              }}
-            />
-          </div>
-        </div>
-        <div className="ppl-wrap">
-          <img src={people} alt={people} />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="main-content">
+			<div className="list-item">
+				<div className="grid-item">
+					<div className="dad item">
+						<img
+							src={img1}
+							alt={img1}
+							onClick={(e) => {
+								clickHandler(e, {
+									actionType: "fireEvent",
+									eventName: page,
+									eventData: {
+										playAudio: audioDad,
+										active: true,
+										imgT1: dadGif,
+									},
+								});
+							}}
+						/>
+					</div>
+					<div className="mom item">
+						<img
+							src={img2}
+							alt={img2}
+							onClick={(e) => {
+								clickHandler(e, {
+									actionType: "fireEvent",
+									eventName: page,
+									eventData: {
+										playAudio: audioMom,
+										active: true,
+										imgT2: momGif,
+									},
+								});
+							}}
+						/>
+					</div>
+					<div className="brother item">
+						<img
+							src={img3}
+							alt={img3}
+							onClick={(e) => {
+								clickHandler(e, {
+									actionType: "fireEvent",
+									eventName: page,
+									eventData: {
+										playAudio: audioBrother,
+										active: true,
+										imgT3: brotherGif,
+									},
+								});
+							}}
+						/>
+					</div>
+					<div className="me item">
+						<img
+							src={img4}
+							alt={img4}
+							onClick={(e) => {
+								clickHandler(e, {
+									actionType: "fireEvent",
+									eventName: page,
+									eventData: {
+										playAudio: audioMe,
+										active: true,
+										imgT4: meGif,
+									},
+								});
+							}}
+						/>
+					</div>
+				</div>
+				<div className="ppl-wrap">
+					<img src={people} alt={people} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Family;
