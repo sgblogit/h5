4fd339bac057b494141bd6283b79963fe2bf0af9
DESKTOP-IQOISR4\Center <sinhworking@gmail.com>
2021-10-26 04:31:11 +0700

    page 17

--- src/pages/Page17/index.js
+++ src/pages/Page17/index.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import audioPlayer from "helper/audioPlayer";
 import audios from "assets/audios/index";
 import images from "assets/images/index";
@@ -8,21 +8,22 @@ import PracticeFind from "components/PracticeFind/index";
 const Page17 = (props) => {
 	const { onPushAction } = props;
 
-	const [active, setActive] = useState("");
-
-	const redBlockRef = useRef();
-
-	const { currentPage, currentStep, currentRecord, prevRecord } = useSelector(
+	const { currentPage, currentStep, currentRecord } = useSelector(
 		(state) => state.app
 	);
 
-	const { playAudio, pauseAudio } = audioPlayer;
+	const { playAudio } = audioPlayer;
 
 	const clickEventName = "page17";
 
 	const clickHandler = (e, op) => {
 		onPushAction(e, op.actionType, op);
 	};
+
+	const [image1, setImage1] = useState(images.common.leftTeacher);
+	const [image2, setImage2] = useState(images.common.rightKid);
+	const [textT1, setTextT1] = useState("");
+	const [textT2, setTextT2] = useState("");
 
 	useEffect(() => {
 		if (currentRecord.length > 0) {
@@ -32,17 +33,21 @@ const Page17 = (props) => {
 				recordEventData.eventPageStep === currentStep &&
 				recordEventData.eventName === clickEventName
 			) {
-				console.log(`runRecordEvent`, recordEventData);
-				if (recordEventData.eventData.active) {
-					setActive("active");
-					//setTimeOutAddClass('red-block','active',2000)
-				} else {
-					setActive("");
-				}
 				if (recordEventData.eventData.playAudio) {
-					//playAudio
 					let audioUrl = audios[recordEventData.eventData.playAudio];
 					playAudio(audioUrl);
+				}
+				if (recordEventData.eventData.imageG1) {
+					setImage1(recordEventData.eventData.imageG1);
+				}
+				if (recordEventData.eventData.textT1) {
+					setTextT1(recordEventData.eventData.textT1);
+				}
+				if (recordEventData.eventData.imageG2) {
+					setImage2(recordEventData.eventData.imageG2);
+				}
+				if (recordEventData.eventData.textT2) {
+					setTextT2(recordEventData.eventData.textT2);
 				}
 			}
 		}
@@ -53,10 +58,8 @@ const Page17 = (props) => {
 	return (
 		<PracticeFind
 			page="page17"
-			// image1={images.page17.teacher17}
-			// image2={images.page17.girl17}
-			object1={images.common.leftTeacher}
-			object2={images.common.rightKid}
+			object1={image1}
+			object2={image2}
 			ball={images.page17.ball17}
 			objectG1={images.common.leftTeacherGif}
 			objectG2={images.common.rightKidGif}
@@ -67,6 +70,8 @@ const Page17 = (props) => {
 			clickHandler={clickHandler}
 			currentPage={currentPage}
 			onPushAction={onPushAction}
+			textT1={textT1}
+			textT2={textT2}
 		/>
 	);
 };
